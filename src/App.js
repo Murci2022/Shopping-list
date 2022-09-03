e03203b96f4b87e727922306197930060f6b1061
Mate Kamaras <mate.kamaras@icloud.com>
2022-09-03 12:55:17 +0200

    changed if statement for true - App.jsx/31

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
-import { useState } from "react";
-
-
-function fetchAPI(){
-const results = await fetch(#)
-
-}
-
-fetch();
+import { useState, useEffect } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [searchItem, setSearchItem] = useState("");
+
+  useEffect(() => {
+    loadItems();
+    async function loadItems() {
+      const response = await fetch(
+        "https://fetch-me.vercel.app/api/shopping/items"
+      );
+      const data = await response.json();
+      setItems(data.data);
+      console.log(data);
+    }
+  }, []);
   return (
-    <div>
-      <h1>Hello Hello</h1>
-    </div>
+    <>
+      <input
+        type="text"
+        placeholder="search"
+        value={searchItem}
+        onChange={(event) => {
+          setSearchItem(event.target.value);
+        }}
+      />
+      {items
+        .filter((item) => {
+          if (searchItem === "") {
+            return true;
+          }
+
+          return item.name.de.toLowerCase().includes(searchItem.toLowerCase());
+        })
+        .map((item) => {
+          return <div key={item._id}>{item.name.de}</div>;
+        })}
+      {/*      {items.filter((item) => {
+        return item.name.de.includes;
+
+    if (searchItem === "") {
+          return true;
+        } else if (item.name.de.includes(searchItem)) {
+          return true;
+        }
+        return false; 
+      })} */}
+
+      <button>Suche</button>
+      {/*  <ul>
+        {items.map((items) => (
+          <li key={items._id}> {items.name.de}</li>
+        ))}
+      </ul> */}
+    </>
   );
 }
 
